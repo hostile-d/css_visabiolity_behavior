8364283fb0b8923f68e20b3bf1f5f5844849d1a0
host <host>
2016-05-17 13:25:28 +0500

    init

---
+++ code.js
@@ -0,0 +1,20 @@
+var child = document.createElement('iframe');
+child.src = 'http://localhost/learn/frame.html';                
+child.width = child.height = '300px';
+child.frameBorder = '0';
+child.style['position'] = 'absolute';
+child.style['top'] = child.style['left'] = '30px';
+child.id = "iframeTest"
+document.body.appendChild(child);
+
+function GetCheckedState () {
+    var input = document.getElementById ("visibilityCheck");
+    var isChecked = input.checked;
+    if (isChecked) {
+    	isChecked = "checked";
+    	child.style['visibility'] = 'visible';
+    } else {
+    	isChecked = "not checked";
+    	child.style['visibility'] = 'hidden';
+    }
+}
