3cfd49234ba4e78e7c067fba676ed84bcd151c8d
host <host>
2016-05-17 15:21:39 +0500

    change visibility target

--- code.js
+++ code.js
@@ -4,17 +4,17 @@ child.width = child.height = '300px';
 child.frameBorder = '0';
 child.style['position'] = 'absolute';
 child.style['top'] = child.style['left'] = '30px';
-child.id = "iframeTest"
-document.body.appendChild(child);
-
+child.id = "iframeTest";
+var wrapper = document.getElementById ("iframeWrapper");
+wrapper.appendChild(child);
 function GetCheckedState () {
     var input = document.getElementById ("visibilityCheck");
     var isChecked = input.checked;
     if (isChecked) {
     	isChecked = "checked";
-    	child.style['visibility'] = 'visible';
+    	wrapper.style['visibility'] = 'visible';
     } else {
     	isChecked = "not checked";
-    	child.style['visibility'] = 'hidden';
+    	wrapper.style['visibility'] = 'hidden';
     }
 }
